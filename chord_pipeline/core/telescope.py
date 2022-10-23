cfcd4e196c3a3f0efbf4db5eb11dbbfc6c18cfd4
Seth Siegel <seth@Seths-Mac-mini.local>
2022-10-22 20:27:40 -0400

    feat(telescope): additional defaults for CHORD64

--- chord_pipeline/core/telescope.py
+++ chord_pipeline/core/telescope.py
@@ -1,15 +1,16 @@
 """Simple models for the CHORD array.
 
 Identical to telescope objects in
-:py:mod:`drift.telescope.custom_disharray.core.PolarisedDishArray` and 
+:py:mod:`drift.telescope.custom_disharray.core.PolarisedDishArray` and
 :py:mod:`drift.telescope.custom_disharray.core.PolarisedDishArraySurvey`,
 but with the following CHORD defaults:
     * Frequency channels set to the CHORD band (300-1500 MHz with 2048 channels)
     * Observatory location set to the approximate location of the CHORD array
       (currently using CHIME location).
-    * A grid array layout with 6.3 m EW spacing and 9.0 m NS spacing for the 
+    * Layout set to a grid with 6.3 m EW spacing and 9.0 m NS spacing for the
       :py:class:`CHORD64` and :py:class:`CHORD64Survey`.
-    * An airy beam pattern with a 6 m diameter dish and -60 dB of cross-polar leakage.
+    * Beam pattern set to Airy with a 6 m diameter dish and -60 dB of cross-polar leakage.
+    * System temperature set to 30 K specification and integration time set to 1 day.
 
 Note that these defaults represent a large telescope object to simulate. For smaller
 scale runs reduce the number of frequency channels using the `channel_range`
@@ -30,19 +31,33 @@ CHORD_ALTITUDE = 555.372  # m
 
 class _CHORD64Defaults(CustomDishArray, config.Reader, metaclass=abc.ABCMeta):
     """Mixin for a CHORD 64-element array.
-    
+
     Identical to :py:class:`drift.telescope.custom_disharray.core.CustomDishArray`
     but with defaults mentioned above.
     """
 
+    # Nominal frequency channels for the intensity mapping backend
     freq_start = config.Property(proptype=float, default=300)
     freq_end = config.Property(proptype=float, default=1500)
     num_freq = config.Property(proptype=int, default=2048)
 
+    # Location of the array
     latitude = config.Property(proptype=float, default=CHORD_LATITUDE)
     longitude = config.Property(proptype=float, default=CHORD_LONGITUDE)
     altitude = config.Property(proptype=float, default=CHORD_ALTITUDE)
 
+    # System temperature
+    tsys_flat = config.Property(proptype=float, default=30.0)
+    ndays = config.Property(proptype=int, default=90)
+
+    # Include auto-correlations
+    auto_correlations = config.Property(proptype=bool, default=True)
+
+    # Diameter of the dish in the E-W and N-S direction
+    min_u = config.Property(proptype=float, default=6.0)
+    min_v = config.Property(proptype=float, default=6.0)
+
+    # Array layout
     layout_spec = config.Property(
         proptype=dict,
         default={
@@ -54,6 +69,7 @@ class _CHORD64Defaults(CustomDishArray, config.Reader, metaclass=abc.ABCMeta):
         },
     )
 
+    # Primary beam
     beam_spec = config.Property(
         proptype=dict,
         default={
