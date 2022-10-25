cee826e90bfc550ca994969196c9debd96aafe10
Seth Siegel <seth@Seths-Mac-mini.local>
2022-10-25 14:08:15 -0400

    feat: add versioneer for versioning

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
 
+import versioneer
+
 setup(
     name="chord_pipeline",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     packages=find_packages(),
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     author="CHORD Collaboration",
     author_email="seth.siegel@mcgill.ca",
     description="CHORD Pipeline",
