70ca08f427d8611da50149e836f5753c043286f7
James Mertens <jbcm627@gmail.com>
2025-12-09 22:20:48 +0000

    fix/update mkenv.sh errors

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# chord_pipeline
-CHORD Pipeline
+# CHORD Pipeline
+
+## Quickstart
+
+To get set up:
+
+1. Prereqs: `python3`, `pip`, `virtualenv`, and SSH access to the GitHub orgs listed in `requirements.txt`.
+2. From the repo root, create and populate the env (clones deps to `venv/src`):  
+   `bash mkvenv.sh`
+   - For a clean env that ignores system packages, add `-i`: `bash mkvenv.sh -i`.
+   - Default install is modern `pip install -e .`; if you need the old `setup.py develop` flow, add `-l`.
+3. Activate the env: `source venv/bin/activate`.
+4. Test the import works: `python -c "import chord_pipeline; print(chord_pipeline.__version__)"`.
+
+If you prefer a different location/name, use `mkvenv.sh -h` for options (`-v` for env path, `-n` for prompt name, `-e` for source path).
--- mkvenv.sh
+++ mkvenv.sh
@@ -5,18 +5,21 @@ venv_name="chord"
 venv_path="./venv"
 code_path="./venv/src"
 ignore_system="false"
+reset_env="false"
+legacy_setup="false"
 
-help_message="$(basename $0) [-n VENV_NAME] [-v VENV_PATH] [-e CODE_PATH] [-i] \n\n
+help_message="$(basename $0) [-n VENV_NAME] [-v VENV_PATH] [-e CODE_PATH] [-i] [-r] \n\n
                 -n  \t Virtual environment name that will appear in your terminal prompt when activated. \n
                     \t\t Defaults to '$venv_name'. \n  
                 -v  \t Path where virtual environment will be installed. \n
                     \t\t Defaults to '$venv_path'. \n
                 -e  \t Path where packages will be cloned for development. \n
                     \t\t Defaults to '$code_path'. \n
-                -i  \t Ignore packages already installed on your system, i.e., install all dependencies. \n"
+                -i  \t Ignore packages already installed on your system, i.e., install all dependencies. \n
+                -r  \t Remove any existing virtual environment at VENV_PATH before creating a new one. \n"
 
 # Parse any options provided by user
-while getopts 'hn:v:e:i' OPTION; do
+while getopts 'hn:v:e:irl' OPTION; do
     case "$OPTION" in
         n)
             venv_name="($OPTARG)"
@@ -34,6 +37,10 @@ while getopts 'hn:v:e:i' OPTION; do
             ignore_system="true"
             ;;
 
+        r)
+            reset_env="true"
+            ;;
+
         h)
             echo -e $help_message >&2
             exit 1
@@ -47,6 +54,11 @@ while getopts 'hn:v:e:i' OPTION; do
 done
 shift "$(($OPTIND -1))"
 
+# Optionally remove existing environment
+if ${reset_env} && [ -d "$venv_path" ]; then
+    rm -rf "$venv_path"
+fi
+
 # Create the virtual environment
 if ${ignore_system}; then
     virtualenv --prompt=$venv_name $venv_path
@@ -56,10 +68,17 @@ fi
 source $venv_path/bin/activate
 
 # Install the packages
-mkdir $code_path
+mkdir -p $code_path
+pip install --use-deprecated=legacy-resolver Cython
 if ${ignore_system}; then
     pip install --use-deprecated=legacy-resolver --src $code_path -r requirements.txt
 else
     pip install --no-build-isolation --use-deprecated=legacy-resolver --src $code_path -r requirements.txt
 fi
-python setup.py develop
+
+# Install this package (modern by default; legacy option retained for compatibility)
+if ${ignore_system}; then
+    pip install --use-deprecated=legacy-resolver -e .
+else
+    pip install --no-build-isolation --use-deprecated=legacy-resolver -e .
+fi
